2adcb807be5d9e589df3cfa56e1557dfc4ebe13e
Lucas Sena <lucasferreiracyrax@gmail.com>
2026-02-09 19:15:57 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/lucassena/workshopmongo/config/Instantiation.java
+++ src/main/java/com/lucassena/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.lucassena.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.lucassena.workshopmongo.domain.Post;
 import com.lucassena.workshopmongo.domain.User;
+import com.lucassena.workshopmongo.repository.PostRepository;
 import com.lucassena.workshopmongo.repository.UserRepository;
 
 @Configuration //sprig entende que isso é uma configuração
@@ -15,16 +19,28 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository; //injeta o user repository para operções com o bd
 	
+	@Autowired
+	private PostRepository postRepository; //injeta o post repository para operções com o bd	
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com"); 
 		User alex = new User(null, "Alex Green", "alex@gmail.com"); 
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia","Acordei feliz hoje!",maria); 
+		
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1,post2));
 		
 	}
 
